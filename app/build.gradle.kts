f5f2173820a064d9f8bfa50f0fe9e5b1ab825bb4
aghissufiantoro <105733271+aghissufiantoro@users.noreply.github.com>
2023-11-01 19:03:10 +0700

    ch5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs.kotlin") version "2.7.2" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     id("kotlin-kapt")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
@@ -40,6 +38,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding; true
+    }
 }
 
 
@@ -47,7 +48,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -55,23 +56,22 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.firebase:firebase-crashlytics-ktx:18.5.0")
 
 
-    implementation ("androidx.room:room-runtime:2.3.0")
-    implementation("com.google.firebase:firebase-crashlytics:18.5.0")
+    implementation("androidx.room:room-runtime:2.6.0")
     implementation("com.google.firebase:firebase-analytics:21.4.0")
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.android.support:support-annotations:28.0.0")
-    annotationProcessor ("androidx.room:room-compiler:2.3.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
     implementation("androidx.cardview:cardview:1.0.0")
 
+    // ViewModel + LiveData
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
-    implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
 
 
     //retrofit & okhttp
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,6 @@ buildscript {
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.2" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
